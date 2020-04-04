10ac2265d1e7994d4eda20a139cfcabb85182b1c
Dingyun Zhu <dzhu13@sheffield.ac.uk>
2020-04-05 00:15:27 +0100

    divide index into model

--- GreenMedia/routes/index.js
+++ GreenMedia/routes/index.js
@@ -3,14 +3,32 @@ var multer = require('multer')
 var upload = multer({dest: 'uploads/'})
 var router = express.Router();
 var release = require('../controllers/release');
-var bodyParser= require("body-parser");
+var bodyParser = require("body-parser");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
-router.post('/', upload.array('files', 3), release.insert);
+router.get('/head', function (req, res, next) {
+    res.render('head', {});
+})
+
+
+router.get('/release', function (req, res, next) {
+    res.render('release', {});
+});
+
+
+router.get('/show-story', function (req, res, next) {
+    res.render('show-story', {});
+})
+
+
+/*post function*/
+
+/*send words & pics*/
+router.post('/release', upload.array('files', 3), release.insert);
 
 
 module.exports = router;
